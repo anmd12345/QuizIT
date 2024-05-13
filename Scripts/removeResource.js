9e82e2b58fd6aa0cef490ab688f8c47a121d4747
anmd12345 <anvohoang98@gmail.com>
2024-05-13 23:53:50 +0700

    add js

---
+++ Scripts/removeResource.js
@@ -0,0 +1,5 @@
+﻿document.addEventListener("keydown", function (event) {
+    if (event.key === "F12" || (event.ctrlKey && event.key === "u")) {
+        console.log("Người dùng đang cố gắng xem mã nguồn!");
+    }
+});
